acc323315e2fbdc0559b3d925c9cdf608f1fa90e
Aidan <aidanwalsh13@gmail.com>
2021-09-29 20:58:52 -0400

    add object oriented data tracking

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-const neededCondCarbon = 50;
-const neededSulphurine = 250;
+const thermicCondensateRecipe = {
+    condensedCarbon: 50,
+    sulphurine: 250
+}
 
-let buttonEl = document.getElementById('Evaluate');
-buttonEl.onclick = evaluate;
+const userIngredients = {
+    condensedCarbon: 0,
+    sulphurine: 0
+}
 
 function evaluate() {
     let result = document.getElementById('Result');
     let sulphurineCount = document.getElementById("SulphurineCount").value;
     let condCarbonCount = document.getElementById("condCarbonCount").value
     
+    userIngredients.sulphurine = sulphurineCount;
+    userIngredients.condensedCarbon = condCarbonCount;
 
     if (!sulphurineCount || !condCarbonCount) {
         result.innerHTML = "Please fill out how much you have of each element, even if it's 0";
-    } else if (condCarbonCount >= neededCondCarbon && sulphurineCount >= neededSulphurine) {
+    } else if (userIngredients.condensedCarbon >= thermicCondensateRecipe.condensedCarbon && userIngredients.sulphurine >= thermicCondensateRecipe.sulphurine) {
         result.innerHTML = "You can do it!";
-    } else if (sulphurineCount < neededSulphurine ) {
+    } else if (userIngredients.condensedCarbon < thermicCondensateRecipe.condensedCarbon && userIngredients.sulphurine < thermicCondensateRecipe.sulphurine) {
+        result.innerHTML = "You need more of both!"
+    } else if (userIngredients.sulphurine < thermicCondensateRecipe.sulphurine) {
         result.innerHTML = "You need more sulphurine!";
-    } else if ( condCarbonCount < neededCondCarbon) {
+    } else if (userIngredients.condensedCarbon < thermicCondensateRecipe.condensedCarbon) {
         result.innerHTML = "You need more Condensed Carbon!";
     }
+}
+
+let buttonEl = document.getElementById('Evaluate');
+buttonEl.onclick = evaluate;
+
+
+const StasisModuleRecipe = {
+    dioxite: 50,
+    phosphorus: 50,
+    paraffinium: 50,
+    faecium: 50,
+    cactusFlesh: 100,
+    frostCrystal: 140,
+    ionizedCobalt: 150,
+    starBulb: 200,
+    solanium: 200,
+    condensedCarbon: 300,
+    gammaRoot: 400,
+    radon: 500,
+    sulphurine: 500,
+    nitrogen: 500
 }
